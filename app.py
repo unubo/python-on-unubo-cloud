8a4966b1e0933314499453353fd46b4c4982e3ec
Marcel Thomas <marcelthomas5@gmail.com>
2019-01-29 08:46:23 +0000

    Added example files

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,7 +8,8 @@ port = int(os.environ.get('PORT', 5000))
 
 @app.route('/')
 def hello():
-    return 'Welcome to Python on Unubo Cloud'
+    # return 'Welcome to Python on Unubo Cloud'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
